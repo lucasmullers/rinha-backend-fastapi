ac9fe21fcf5b296c81ccc87020f5d42192048648
lucasmullers <lucas.mullers@gmail.com>
2023-08-21 20:25:49 -0300

    feat: add script to drop and create table on postgres Dockerfile

--- postgres/create-databases.sh
+++ postgres/create-databases.sh
@@ -13,10 +13,28 @@ function create_user_and_database() {
 EOSQL
 }
 
-if [ -n "$POSTGRES_DBS" ]; then
-	echo "Creating DB(s): $POSTGRES_DBS"
-	for db in $(echo $POSTGRES_DBS | tr ',' ' '); do
-		create_user_and_database $db
-	done
-	echo "Multiple databases created"
-fi
+function create_table() {
+	local database=$1
+	echo "Creating table '$database'"
+	psql -v ON_ERROR_STOP=1 -d $database -U "$POSTGRES_USER" <<-EOSQL
+		DROP TABLE IF EXISTS "public"."pessoas";
+		CREATE TABLE "public"."pessoas"(
+			"stack" varchar[],
+			"id" uuid NOT NULL,
+			"apelido" character varying(32) NOT NULL,
+			"nome" character varying(100) NOT NULL,
+			"nascimento" character varying(10) NOT NULL
+		);
+		CREATE UNIQUE INDEX pessoas_pkey ON public.pessoas USING btree (id);
+		CREATE UNIQUE INDEX pessoas_apelido_key ON public.pessoas USING btree (apelido);
+		CREATE INDEX ix_pessoas_id ON public.pessoas USING btree (id);
+EOSQL
+}
+
+
+echo "Creating DB(s): rinha"
+for db in $(echo "rinha" | tr ',' ' '); do
+	create_user_and_database $db
+	create_table $db
+done
+echo "Multiple databases created"
